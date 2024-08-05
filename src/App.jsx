09330023ea9f9ead564ba00f216c2b136cb1a109
Varun <varunjethani@outlook.com>
2024-08-05 08:53:06 +0530

    add bg

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { motion, useScroll, useTransform } from "framer-motion";
 import { useRef } from "react";
+import bgimg from "./assets/bgimg.png";
+import "./App.css"
 const App = () => {
   const targetRef = useRef(null);
   const { scrollYProgress } = useScroll({
@@ -8,12 +10,14 @@ const App = () => {
   });
   const x = useTransform(scrollYProgress, [0, 1], ["0%", "-100%"]);
   return (
-    <div ref={targetRef} className="h-[300vh] relative bg-neutral-900 ">
+    <div ref={targetRef} className="h-[300vh] relative bg2 ">
       <div className="sticky top-0 h-screen flex items-center overflow-hidden">
-        <motion.div style={{ x }} className="flex gap-0">
-          <div className="h-screen w-screen bg-red-600"></div>
-          <div className="h-screen w-screen bg-green-600"></div>
-          <div className="h-screen w-screen bg-blue-600"></div>
+        <motion.div style={{ x }} className="flex gap-0 ">
+          <div className="h-screen w-screen bg ">
+            <h1 className="text-white">Hello</h1>
+          </div>
+          <div className="h-screen w-screen bg2"></div>
+          <div className="h-screen w-screen bg"></div>
           <div className="h-screen w-screen bg-gray-600"></div>
         </motion.div>
       </div>
